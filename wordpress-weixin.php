dc0178e528b5f7a4260f6355e2cb6de21d1e29f8
polin.wei <polin.wei@gmail.com>
2019-06-14 09:03:33 +0800

    update Author URI

--- wordpress-weixin.php
+++ wordpress-weixin.php
@@ -4,7 +4,7 @@
  * Description: 微信公眾號-發送訊息系統平台
  * Version: 1.0.0
  * Author: Polin WEI
- * Author URI: http://
+ * Author URI: https://github.com/polinwei/wordpress-weixin
  * License: MIT
  * Text Domain: Weixin-Wechat
  */
